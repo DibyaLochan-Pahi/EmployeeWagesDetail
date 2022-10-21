2a557d2261d67c1e194c9c944a4260eaeeba3e15
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-10-21 18:58:58 +0530

    Added UC2 Program of EmpWage

--- EmployeeWagesDetail/Program.cs
+++ EmployeeWagesDetail/Program.cs
@@ -2,21 +2,28 @@
 
 namespace EmployeeWagesDetail
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            int ispresent = 1;
-            Random ran = new Random();
-            int checkEmployee = ran.Next(0, 2);
-            if (ispresent == checkEmployee)
+            int isFullTime = 1;
+            int empWagePerHour = 20;
+            int empWages = 0;
+            int empHour = 0;
+            Random random = new Random();
+            int empStatus = random.Next(0, 2);
+            if (empStatus == isFullTime)
             {
                 Console.WriteLine("Employee is Present");
+                empHour = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHour = 0;
             }
+            empWages = empWagePerHour * empHour;
+            Console.WriteLine("Employee Wages is " + empWages);
         }
     }
 }
