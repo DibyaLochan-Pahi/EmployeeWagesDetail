b3b246c59328fe68336ca3a77f2450e91e8a1a0c
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-10-21 18:46:07 +0530

    Added Employee Wage Program to github

---
+++ EmployeeWagesDetail/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployeeWagesDetail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ispresent = 1;
+            Random ran = new Random();
+            int checkEmployee = ran.Next(0, 2);
+            if (ispresent == checkEmployee)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
