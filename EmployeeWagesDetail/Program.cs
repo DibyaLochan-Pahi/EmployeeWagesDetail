cc400f7feb96cbfa4325fdcf1e577d1689ba723b
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-10-21 19:06:26 +0530

    Added Half Day in EmpWage

--- EmployeeWagesDetail/Program.cs
+++ EmployeeWagesDetail/Program.cs
@@ -7,15 +7,21 @@ namespace EmployeeWagesDetail
         static void Main(string[] args)
         {
             int isFullTime = 1;
+            int isHalfTime = 2;
             int empWagePerHour = 20;
             int empWages = 0;
             int empHour = 0;
             Random random = new Random();
-            int empStatus = random.Next(0, 2);
+            int empStatus = random.Next(0, 3);
             if (empStatus == isFullTime)
             {
                 Console.WriteLine("Employee is Present");
                 empHour = 8;
+            }
+            else if (empStatus == isHalfTime)
+            {
+                Console.WriteLine("Employee is Half Time");
+                empHour = 4;
             }
             else
             {
