731b446107957d4b26f45b2edf193d7c8755a24d
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-10-22 16:26:49 +0530

    Added Employee Wages For Month And Hour

--- EmployeeWagesDetail/Program.cs
+++ EmployeeWagesDetail/Program.cs
@@ -9,25 +9,39 @@ namespace EmployeeWagesDetail
             int empWagePerHour = 20;
             int empHour = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empStatus = random.Next(0, 3);
-            switch (empStatus)
+            int maxDay = 20;
+            int maxHour = 100;
+            int totalEmpWage = 0;
+            int totalEmpHour = 0;
+            int totalEmpDay = 0;
+            while (totalEmpHour < maxHour && totalEmpDay < maxDay)
             {
-                case 1:
-                    Console.WriteLine("Employee is Present");
-                    empHour = 8;
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is Half Time");
-                    empHour = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent");
-                    empHour = 0;
-                    break;
+                Random random = new Random();
+                int empStatus = random.Next(0, 3);
+                switch (empStatus)
+                {
+                    case 1:
+                        Console.WriteLine("Employee is Present");
+                        empHour = 8;
+                        break;
+                    case 2:
+                        Console.WriteLine("Employee is Half Time");
+                        empHour = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        empHour = 0;
+                        break;
+                }
+                empWage = empHour * empWagePerHour;
+                totalEmpWage = totalEmpWage + empWage;
+                totalEmpHour = totalEmpHour + empHour;
+                Console.WriteLine("Employee Wage : " + empWage);
             }
-            empWage = empWagePerHour * empHour;
-            Console.WriteLine("The Employee Wage" + empWage);
+            Console.WriteLine("Total Employee Wages For a month: " + totalEmpWage);
+            Console.WriteLine("Total Employee Hour " + totalEmpHour);
+
+
         }
     }
 }
