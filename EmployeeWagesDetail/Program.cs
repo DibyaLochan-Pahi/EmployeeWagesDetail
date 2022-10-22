bfe1d1e3372c1a41a577df74ff4f25ec9d21b383
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-10-22 16:08:40 +0530

    Adeed Month Wages of Employee

--- EmployeeWagesDetail/Program.cs
+++ EmployeeWagesDetail/Program.cs
@@ -9,25 +9,34 @@ namespace EmployeeWagesDetail
             int empWagePerHour = 20;
             int empHour = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empStatus = random.Next(0, 3);
-            switch (empStatus)
+            int maxDay = 20;
+            int totalEmpWage = 0;
+            for (int day = 0; day < maxDay; day++)
             {
-                case 1:
-                    Console.WriteLine("Employee is Present");
-                    empHour = 8;
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is Half Time");
-                    empHour = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent");
-                    empHour = 0;
-                    break;
+                Random random = new Random();
+                int empStatus = random.Next(0, 3);
+                switch (empStatus)
+                {
+                    case 1:
+                        Console.WriteLine("Employee is Present");
+                        empHour = 8;
+                        break;
+                    case 2:
+                        Console.WriteLine("Employee is Half Time");
+                        empHour = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        empHour = 0;
+                        break;
+                }
+                empWage = empHour * empWagePerHour;
+                totalEmpWage = totalEmpWage + empWage;
+                Console.WriteLine("Employee Wage : " + empWage);
             }
-            empWage = empWagePerHour * empHour;
-            Console.WriteLine("The Employee Wage" + empWage);
+            Console.WriteLine("Total Employee Wages For 20 Days: " + totalEmpWage);
+
+
         }
     }
 }
